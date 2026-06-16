f72b0b7be012b2dab3af1733298147d500586cfa
Han Seong Ju <gkstjdwntl@gmail.com>
2026-06-16 12:30:37 +0900

    spring

---
+++ ch03/src/main/java/org/example/ch03/DTO/UserDTO.java
@@ -0,0 +1,20 @@
+package org.example.ch03.DTO;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+// @RequiredArgsConstructor
+@Builder
+public class UserDTO {
+    private String userid;
+    private String name;
+    private String birth;
+    private int age;
+
+
+
+}
